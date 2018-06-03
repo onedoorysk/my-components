70a6fb4382c7c02ccb3308006c7e929f0fbc7da1
onedoorysk <rysk1008@gmail.com>
2018-06-03 22:10:45 +0900

    add transition moving

--- navigation/script.js
+++ navigation/script.js
@@ -1,21 +1,44 @@
 (() => {
   const $navLink = document.getElementsByClassName('nav-link')
   const $navCursol = document.querySelector('.nav-cursol')
-  // 画面表示後のカーソル初期位置を設定
+  let animation
+
   const initFirstPosition = () => {
     $navCursol.style.width = $navLink[0].getBoundingClientRect().width + 'px'
+    $navCursol.style.left = '0px'
   }
-  // ホバー時のカーソル位置とサイズ調整
-  const changeCursolPosition = ($navLink, cursolLeftValue) => {
-    $navCursol.style.width = $navLink.getBoundingClientRect().width + 'px'
-    $navCursol.style.left = cursolLeftValue + 'px'
+  const registChangeCursolEvent = () => {
+    for (let i=0; i<$navLink.length; i++) {
+      // 各メニューの位置とカーソルの初期位置の差分を計算。
+      const cursolPositionValue = $navLink[i].getBoundingClientRect().left - $navCursol.getBoundingClientRect().left
+      
+      let cursolProperty = {
+        targetWidth: $navLink[i].getBoundingClientRect().width + 'px',
+        targetPosition: cursolPositionValue
+      }
+
+      $navLink[i].addEventListener('mouseover', () => {
+        clearTimeout(animation)
+
+        const beforeCursolPosition = parseFloat($navCursol.style.left)
+        if (beforeCursolPosition < cursolProperty.targetPosition) {
+          $navCursol.style.left = 15 + cursolProperty.targetPosition + 'px'
+        } else {
+          $navCursol.style.left = -15 + cursolProperty.targetPosition + 'px'
+        }
+        animation = setTimeout(() => {
+          $navCursol.style.left = cursolProperty.targetPosition + 'px'
+        }, 500)
+
+        $navCursol.style.width = cursolProperty.targetWidth
+        animation()
+      })
+    }
   }
 
+  // 画面表示後のカーソル初期位置を設定
   initFirstPosition()
-
-  for (let i=0; i<$navLink.length; i++) {
-    const cursolLeftValue = $navLink[i].getBoundingClientRect().left - $navCursol.getBoundingClientRect().left 
-    $navLink[i].addEventListener('mouseover', () => changeCursolPosition($navLink[i], cursolLeftValue))
-  }
+  // カーソルの状態変更イベント設定
+  registChangeCursolEvent()
 })()
 
